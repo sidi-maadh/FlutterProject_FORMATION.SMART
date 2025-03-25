b9ac2f5038f8cb6e6d2595657b05d621f2737cca
Sidi Chrif Ahmed Maadh <155539380+sidi-maadh@users.noreply.github.com>
2025-03-25 11:35:40 +0000

    Add files via upload

---
+++ First_App/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/gardein_contianer.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(body: GradeintContainer(Colors.blue, Colors.green)),
+    ),
+  );
+}
